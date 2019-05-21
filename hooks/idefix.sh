ddb52c3ddb0da0a6e94339b6a9714bdcf3daf51e
i810697 <mathieu.sauve@sap.com>
2019-05-21 16:41:59 -0400

    testing

--- hooks/idefix.sh
+++ hooks/idefix.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+# Setting up some colours for a sexy output
+red=$'\e[0;31m'
+green=$'\e[0;32m'
+light_cyan=$'\e[1;36m'
+yellow=$'\e[0;33m'
+nocolor=$'\e[0m'
+
 # Run helm lint on the chart path.
 # A typical helm chart directory structure looks as follows:
 #
@@ -33,102 +40,25 @@ set -e
 # workaround to allow GitHub Desktop to work, add this (hopefully harmless) setting here.
 export PATH=$PATH:/usr/local/bin
 
-# Take the current working directory to know when to stop walking up the tree
-readonly cwd_abspath="$(realpath "$PWD")"
-
-# https://stackoverflow.com/a/8574392
-# Usage: contains_element "val" "${array[@]}"
-# Returns: 0 if there is a match, 1 otherwise
-contains_element() {
-  local -r match="$1"
-  shift
-
-  for e in "$@"; do
-    if [[ "$e" == "$match" ]]; then
-      return 0
-    fi
-  done
-  return 1
-}
-
-# Only log debug statements if PRECOMMIT_DEBUG environment variable is set.
-# Log to stderr.
-debug() {
-  if [[ ! -z $PRECOMMIT_DEBUG ]]; then
-    >&2 echo "$@"
-  fi
-}
-
-# Recursively walk up the tree until the current working directory and check if the changed file is part of a helm
-# chart. Helm charts have a Chart.yaml file.
-chart_path() {
-  local -r changed_file="$1"
-
-  # We check both the current dir as well as the parent dir, in case the current dir is a file
-  local -r changed_file_abspath="$(realpath "$changed_file")"
-  local -r changed_file_dir="$(dirname "$changed_file_abspath")"
-
-  debug "Checking directory $changed_file_abspath and $changed_file_dir for Chart.yaml"
-
-  # Base case: we have walked to the top of dir tree
-  if [[ "$changed_file_abspath" == "$cwd_abspath" ]]; then
-    debug "No chart path found"
-    echo ""
-    return 0
-  fi
-
-  # The changed file is itself the helm chart indicator, Chart.yaml
-  if [[ "$(basename "$changed_file_abspath")" == "Chart.yaml" ]]; then
-    debug "Chart path found: $changed_file_dir"
-    echo "$changed_file_dir"
-    return 0
-  fi
-
-  # The changed_file is the directory containing the helm chart package file
-  if [[ -f "$changed_file_abspath/Chart.yaml" ]]; then
-    debug "Chart path found: $changed_file_abspath"
-    echo "$changed_file_abspath"
-    return 0
-  fi
-
-  # The directory of changed_file is the directory containing the helm chart package file
-  if [[ -f "$changed_file_dir/Chart.yaml" ]]; then
-    debug "Chart path found: $changed_file_dir"
-    echo "$changed_file_dir"
-    return 0
-  fi
-
-  # None of the above, so recurse and do again in the parent dir
-  chart_path "$changed_file_dir"
-}
-
-# An array to keep track of which charts we already linted
-seen_chart_paths=()
 
 for file in "$@"; do
-  debug "Checking $file"
-  file_chart_path=$(chart_path "$file")
-  debug "Resolved $file to chart path $file_chart_path"
+  if [[ $"basename $file" == "values.yaml" ]]; then
+    # Unset previously parsed variables from the values file of the lms-master-chart
+    for image in $(grep -E "repository:" "${file}" | awk '{print $2}'); do
+      echo "${light_cyan}Image: ${image}${nocolor}"
 
-  # The chart values.yaml file may not have all the values defined to enforce default values, which will cause the
-  # linter to fail. To support this, this pre-commit hook looks for a special values file called `linter_values.yaml`
-  # which should define the additional values that will be fed to the linter.
-  if [[ -f "$file_chart_path/linter_values.yaml" ]]; then
-    linter_values_arg="$file_chart_path/linter_values.yaml"
-  else
-    linter_values_arg=""
-  fi
+      latest_tag=$(gcloud container images list-tags "${image}" | grep -v "TAGS" | awk '{ print $2}' | sort -rV | head -n 1)
+      echo "${light_cyan}Latest tag: ${green}${latest_tag}${nocolor}"
+      current_tag=$(grep -E "(^|\s)${image}($|\s)" -A 2 "${file}" | grep -v "#" | grep -v "${image}" | awk '{print $2}')
+      echo "${light_cyan}Current tag: ${yellow}${current_tag}${nocolor}"
 
-  if [[ ! -z "$file_chart_path" ]]; then
-    if contains_element "$file_chart_path" "${seen_chart_paths[@]}"; then
-      debug "Already linted $file_chart_path"
-    elif [[ -z "$linter_values_arg" ]]; then
-      helm lint "$file_chart_path"
-      seen_chart_paths+=( "$file_chart_path" )
-    else
-      # Combine both linter_values.yaml and values.yaml
-      helm lint -f "$file_chart_path/values.yaml" -f "$linter_values_arg" "$file_chart_path"
-      seen_chart_paths+=( "$file_chart_path" )
-    fi
+      if [[ "${latest_tag}" != "${current_tag}" ]]; then
+        echo "${red}Current tag is not up to date${nocolor}"
+        exit 0
+      else
+        echo "${green}Current tag is up to date${nocolor}"
+      fi
+      echo "========================="
+    done
   fi
 done
